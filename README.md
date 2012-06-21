c774b70ea1604e5e1d504b28b3caf02d5f9bbba8
Lefteris Georgatos <lefteris.georgatos@gmail.com>
2012-06-21 23:58:57 +0300

    Add readme adn fixes locale array helper

---
+++ README.md
@@ -0,0 +1,46 @@
+globalize3_helpers
+==================
+
+Form helpers for globalize3.
+
+## Install globalize3_helpers
+
+###Installing gem:
+
+   `gem install globalize3_helpers` 
+
+## Using it
+
+### Model:
+  Inside your model:
+
+  `accepts_nested_attributes_for :translations`
+
+### View:
+  Supports form_for and simple_form
+  
+  With array of locales:
+  (haml syntax)
+
+    - f.globalize_fields_for_locales [ :en, :el ] do |l|
+      = l.input :name, input_html: { :class => :span5 }
+      = l.input :description, as: :text, input_html: { :class => 'span6 without_radius' }
+
+  With single locale:
+  (haml syntax)
+
+    - f.globalize_fields_for_locale :en do |l|
+      = l.input :name, input_html: { :class => :span5 }
+      = l.input :description, as: :text, input_html: { :class => 'span6 without_radius' }
+
+### Contributing 
+  1. Fork the code, add more helpers
+  2. Most important contribution would be testing 
+
+### Notice
+  I know the current code has a lot of duplication, in the next days i will fix it.
+
+### Copyrights
+Copyright (c) 2012 Lefteris Georgatos. See LICENSE.txt for
+further details.
+
--- lib/helpers/helper.rb
+++ lib/helpers/helper.rb
@@ -1,7 +1,27 @@
 module ActionView
   module Helpers
     class FormBuilder
-      def globalize_fields_for(locale, *args, &proc)
+
+      def globalize_fields_for_locales(locales = [], *args, &proc)
+        raise ArgumentError, "Missing block" unless block_given?
+        globalize_fields_for(locales.first.to_s, *args, &proc) if locales.count <= 1
+        translations_fields = []
+
+        locales.each do |locale|
+          @index = @index ? @index + 1 : 1
+          object_name = "#{@object_name}[translations_attributes][#{@index}]"
+          object = @object.translations.find_by_locale locale.to_s
+          @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
+          @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
+          if @template.respond_to? :simple_fields_for
+            @template.concat @template.simple_fields_for(object_name, object, *args, &proc)
+          else
+            @template.concat @template.fields_for(object_name, object, *args, &proc)
+          end
+        end
+      end
+
+      def globalize_fields_for_locale(locale, *args, &proc)
         raise ArgumentError, "Missing block" unless block_given?
         @index = @index ? @index + 1 : 1
         object_name = "#{@object_name}[translations_attributes][#{@index}]"
@@ -14,6 +34,7 @@ module ActionView
           @template.fields_for(object_name, object, *args, &proc)
         end
       end
+
     end
   end
 end
