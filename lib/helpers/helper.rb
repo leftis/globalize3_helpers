08fc82209c4603b28ab9bb00c0f7b4170eda6a89
Luis Doubrava <luis@cg.nl>
2012-10-04 08:21:37 +0200

    instantiate new object if translation doesn't exist

--- lib/helpers/helper.rb
+++ lib/helpers/helper.rb
@@ -25,9 +25,9 @@ module ActionView
         raise ArgumentError, "Missing block" unless block_given?
         @index = @index ? @index + 1 : 1
         object_name = "#{@object_name}[translations_attributes][#{@index}]"
-        object = @object.translations.find_by_locale locale.to_s
-        @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
-        @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
+        object = @object.translations.find_by_locale(locale.to_s) || @object.translations.new(:locale => locale)
+        @template.concat(@template.hidden_field_tag("#{object_name}[id]", object.id)) unless object.new_record?
+        @template.concat(@template.hidden_field_tag("#{object_name}[locale]", locale))
         if @template.respond_to? :simple_fields_for
           @template.concat @template.simple_fields_for(object_name, object, *args, &proc)
         else
