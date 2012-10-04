0bc3a04c2d85aeb3073359cdf986106672e26be2
Luis Doubrava <luis@cg.nl>
2012-10-04 08:22:09 +0200

    remove duplicate code and call globalize_fields_for_locale from globalize_fields_for_locales

--- lib/helpers/helper.rb
+++ lib/helpers/helper.rb
@@ -8,16 +8,7 @@ module ActionView
         translations_fields = []
 
         locales.each do |locale|
-          @index = @index ? @index + 1 : 1
-          object_name = "#{@object_name}[translations_attributes][#{@index}]"
-          object = @object.translations.find_by_locale locale.to_s
-          @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
-          @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
-          if @template.respond_to? :simple_fields_for
-            @template.concat @template.simple_fields_for(object_name, object, *args, &proc)
-          else
-            @template.concat @template.fields_for(object_name, object, *args, &proc)
-          end
+          globalize_fields_for_locale(locale, *args, &proc)
         end
       end
 
