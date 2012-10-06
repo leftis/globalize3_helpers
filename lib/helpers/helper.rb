696c7e9fc500131d8b8752ea3fadc11a94b031ed
Lefteris Georgatos <lefteris.georgatos@gmail.com>
2012-10-06 09:16:00 +0300

    Changes formbuilder from module  to class

--- lib/helpers/helper.rb
+++ lib/helpers/helper.rb
@@ -1,25 +1,27 @@
-module ActionView::Helpers::FormBuilder
+module ActionView::Helpers
+  class FormBuilder
   
-  def globalize_fields_for_locale(locale, *args, &proc)
-    raise ArgumentError, "Missing block" unless block_given?
-    @index = @index ? @index + 1 : 1
-    object_name = "#{@object_name}[translations_attributes][#{@index}]"
-    object = @object.translations.find_by_locale(locale.to_s) || @object.translations.new(:locale => locale)
-    @template.concat(@template.hidden_field_tag("#{object_name}[id]", object.id)) unless object.new_record?
-    @template.concat(@template.hidden_field_tag("#{object_name}[locale]", locale))
-    if @template.respond_to? :simple_fields_for
-      @template.concat @template.simple_fields_for(object_name, object, *args, &proc)
-    else
-      @template.concat @template.fields_for(object_name, object, *args, &proc)
+    def globalize_fields_for_locale(locale, *args, &proc)
+      raise ArgumentError, "Missing block" unless block_given?
+      @index = @index ? @index + 1 : 1
+      object_name = "#{@object_name}[translations_attributes][#{@index}]"
+      object = @object.translations.find_by_locale(locale.to_s) || @object.translations.new(:locale => locale)
+      @template.concat(@template.hidden_field_tag("#{object_name}[id]", object.id)) unless object.new_record?
+      @template.concat(@template.hidden_field_tag("#{object_name}[locale]", locale))
+      if @template.respond_to? :simple_fields_for
+        @template.concat @template.simple_fields_for(object_name, object, *args, &proc)
+      else
+        @template.concat @template.fields_for(object_name, object, *args, &proc)
+      end
     end
-  end
   
-  alias_method :globalize_fields_for_locale, :globalize_fields_for_locales
+    alias_method :globalize_fields_for_locale, :globalize_fields_for_locales
     
-  def globalize_fields_for_locales(locales = [], *args, &proc)
-    locales.each do |locale|
-      super(locale, *args, &proc)
+    def globalize_fields_for_locales(locales = [], *args, &proc)
+      locales.each do |locale|
+        super(locale, *args, &proc)
+      end
     end
+    
   end
-
 end
